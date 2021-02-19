178fd75cf6c5e82660ed35cd2caf455d3b562374
alia-as <aliabdollahiasl@gmail.com>
2021-02-19 16:03:19 +0330

    second commit

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def home_view(*args,**kwargs):
+def home_view(request,*args,**kwargs):
     return HttpResponse("<h1>That's awesome</h1>")
+def student_view(request,*args,**kwargs):
+    return HttpResponse("<h1>Hi Students</h1>")
+def teacher_view(request,*args,**kwargs):
+    return HttpResponse("<h1>Hello Mr. Nowzari</h1>")
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import home_view
+from pages.views import home_view , student_view , teacher_view
 urlpatterns = [
     path('', home_view),
+    path('student', student_view),
+    path('teacher', teacher_view),
     path('admin/', admin.site.urls),
 ]
